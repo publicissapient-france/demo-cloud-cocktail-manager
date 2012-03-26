bac7c36ad993e607981ae28c38e9574ec32a6514
Cyrille Le Clerc <cleclerc@xebia.fr>
2012-03-26 09:50:12 +0200

    work in progress

---
+++ src/main/java/fr/xebia/cocktail/MailService.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2008-2012 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.cocktail;
+
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Strings;
+
+@Service
+public class MailService {
+
+    private Session mailSession;
+
+    protected final Logger auditLogger = LoggerFactory.getLogger("audit");
+
+    protected InternetAddress fromAddress;
+
+    @Inject
+    public MailService(@Named("smtpProperties") Properties smtpProperties) throws MessagingException {
+
+        if (Strings.isNullOrEmpty(smtpProperties.getProperty("mail.username"))) {
+            mailSession = Session.getInstance(smtpProperties);
+        } else {
+            final String username = smtpProperties.getProperty("mail.username");
+            final String password = smtpProperties.getProperty("mail.password");
+
+            mailSession = Session.getInstance(smtpProperties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+        }
+
+        fromAddress = new InternetAddress(smtpProperties.getProperty("mail.from"));
+    }
+
+    public void sendCocktail(Cocktail cocktail, String recipient, String cocktailPageUrl) throws MessagingException {
+
+        Message msg = new MimeMessage(mailSession);
+
+        msg.setFrom(fromAddress);
+        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+        msg.setSubject("[Cocktail] " + cocktail.getName());
+        String message = cocktail.getName() + "\n" //
+                + "--------------------\n" //
+                + "\n" //
+                + Strings.nullToEmpty(cocktail.getInstructions()) + "\n" //
+                + "\n" //
+                + cocktailPageUrl;
+        msg.setContent(message, "text/plain");
+
+        Transport.send(msg);
+        auditLogger.info("Sent to {} cocktail '{}'", recipient, cocktail.getName());
+
+    }
+
+}
---
+++ src/test/java/fr/xebia/cocktail/MongoDBCocktailRepositoryTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2008-2012 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.cocktail;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.bson.types.ObjectId;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mongodb.DBObject;
+
+public class MongoDBCocktailRepositoryTest {
+    static private MongoDBCocktailRepository cocktailRepository;
+
+    @BeforeClass
+    public static void before() throws Exception {
+        cocktailRepository = new MongoDBCocktailRepository("mongodb://localhost:27017/devoxxfr-demo", "http://localhost:8983/solr/");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        cocktailRepository.preDestroy();
+    }
+
+    @Test
+    public void testInsertUpdateAndDelete() {
+        Cocktail sexOnTheBeach = buildLongIslandCocktail();
+
+        cocktailRepository.insert(sexOnTheBeach);
+        assertNotNull("objectId has not been generated", sexOnTheBeach.getObjectId());
+
+        // boolean deleted = cocktailDao.delete(sexOnTheBeach);
+        // assertTrue("cocktail " + sexOnTheBeach + " was not deleted", deleted);
+    }
+
+    @Test
+    public void testBsonMapping() {
+
+        Cocktail sexOnTheBeach = buildLongIslandCocktail();
+        ObjectId objectId = ObjectId.get();
+        sexOnTheBeach.setObjectId(objectId);
+
+        DBObject sexOnTheBeachAsDbObject = cocktailRepository.toBson(sexOnTheBeach);
+
+        Cocktail actual = cocktailRepository.fromBson(sexOnTheBeachAsDbObject);
+
+        assertEquals(objectId, actual.getObjectId());
+        assertEquals("Sex On The Beach", actual.getName());
+        assertEquals(5, actual.getIngredients().size());
+        assertArrayEquals(new String[] { "vodka", "peach schnapps (archers)", "orange juice", "cranberry juice", "raspberry syrup" },
+                actual.getIngredientNames().toArray());
+    }
+
+    protected Cocktail buildSexOnTheBeachCocktail() {
+        Cocktail sexOnTheBeach = new Cocktail()
+                .withName("Sex On The Beach")
+                .withIngredient("1 shot", "vodka")
+                .withIngredient("1 shot", "peach schnapps (archers)")
+                .withIngredient("200 ml", "orange juice")
+                .withIngredient("200 ml", "cranberry juice")
+                .withIngredient("2 shots", "raspberry syrup")
+                .withInstructions(
+                        "Add ice to glass pour in shot of vodka add peach shnapps mix with orange, cranberry and raspberry\n" //
+                                + "\n" //
+                                + "Serve with an ubrella and a mixer stick and a fancy straw and an orange slice on side of "
+                                + "glass this one is gorgeous can't believe you don't already have it on here!");
+        return sexOnTheBeach;
+    }
+
+    protected Cocktail buildLongIslandCocktail() {
+        Cocktail longIslandIcedTea = new Cocktail()
+                .withName("Long Island Iced tea")
+                .withIngredient("1 Measure", "vodka")
+                .withIngredient("1 Measure", "gin")
+                .withIngredient("1 Measure", "white rum")
+                .withIngredient("1 Measure", "tequila")
+                .withIngredient("1 Measure", "triple sec")
+                .withIngredient("3 measures", "orange juice")
+                .withIngredient("to topp up the glass", "coke")
+                .withInstructions(
+                        "In a tall glass , add ice and all the ingredients and stir well. It should have the appearance of cloudy tea. Top with a piece of lemon\n"
+                                + "\n"
+                                + "Very yummy & very very decieving. It will get you hammered after only about 2 so drink with caution");
+        return longIslandIcedTea;
+    }
+
+    @Test
+    public void testAutoComplete() {
+        Collection<String> words = cocktailRepository.autocompleteCocktailNameWords("lon");
+        assertArrayEquals(new String[] { "long" }, words.toArray());
+    }
+}
